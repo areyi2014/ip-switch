d20a0e774366fdba55b82563a4ad361959387011
areyi <chenyi7941@163.com>
2026-09-09 22:29:51 +0800

    Extend translation map for multiline subjects and BOM edge cases

--- .workbuddy/translate-msg.cjs
+++ .workbuddy/translate-msg.cjs
@@ -3,7 +3,8 @@ const [, , mapPath] = process.argv;
 const pairs = fs.readFileSync(mapPath, 'utf8')
   .split('\n')
   .filter(l => l.includes('\t'))
-  .map(l => { const i = l.indexOf('\t'); return [l.slice(0, i), l.slice(i + 1)]; });
+  .map(l => { const i = l.indexOf('\t'); return [l.slice(0, i), l.slice(i + 1)]; })
+  .map(([from, to]) => [from.split('\\n').join('\n'), to.split('\\n').join('\n')]);
 let data = '';
 process.stdin.on('data', d => { data += d; });
 process.stdin.on('end', () => {
